d0c07c81b9502ee07b57f1524787bd4286aee451
nurdaulet <nurdaulet1312@gmail.com>
2021-05-07 10:29:38 +0400

    follow, following functionality

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,8 +1,7 @@
-import React, { SyntheticEvent, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Icon, Item, Label, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
-import { useStore } from '../../../app/stores/store';
 import { format } from 'date-fns';
 import ActivityListItemAttendee from './ActivityListItemAttendee';
 
